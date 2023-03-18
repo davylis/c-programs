eaab7ad7f5e85b6567c2dd2b6fbdeaa55be6ad82
Liisa Davydov <122706955+davylis@users.noreply.github.com>
2023-03-18 17:07:19 +0200

    Create reading_and_printing.c

---
+++ reading_and_printing.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+	int integer, square;
+	printf("Enter an integer: ");
+	scanf("%d", &integer);
+	square = integer * integer;
+	printf("The square of the number you entered is %d", square);
+	return 0;
+}
+	
