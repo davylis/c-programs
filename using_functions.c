0657b03410d69bee92f687393379ce14ac12ceb4
davylis <liisamuttonen@gmail.com>
2023-03-20 20:31:40 +0200

    using functions: modify to use loop

--- using_functions.c
+++ using_functions.c
@@ -5,17 +5,17 @@ int smallest(int a, int b, int c);
 
 int main()
 {
-    int num_one, num_two, num_three, max, min;
-    printf("Enter the 1. number:");
-    scanf("%d", &num_one);
-    printf("Enter the 2. number:");
-    scanf("%d", &num_two);
-    printf("Enter the 3. number:");
-    scanf("%d", &num_three);
-    max = largest (num_one, num_two, num_three);
-    min = smallest (num_one, num_two, num_three);
+    int numbers[3], max, min;
+    for(int i=1; 3 >= i; i++ )
+    {
+        printf("Enter the %d number:\n", i);
+        scanf("%d", &numbers[i-1]);
+    }
+    max = largest (numbers[0], numbers[1], numbers[2]);
+    min = smallest(numbers[0], numbers[1], numbers[2]);
     printf("Among the numbers you entered,\n"
     "the largest was %d and the smallest was %d.\n",max, min );
+    
     return 0;
 }
 
