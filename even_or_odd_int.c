906eadf5d6821f9ae2b4a6d1be01d6490bf79f23
davylis <liisamuttonen@gmail.com>
2023-03-23 22:09:11 +0200

    Even or odd program

---
+++ even_or_odd_int.c
@@ -0,0 +1,19 @@
+int even_or_odd (int a)
+{ 
+	int remainder = a % 2;
+	if (remainder == 1){
+		return 1;
+	} else {
+	    return 0;
+	}
+}
+
+int main ()
+{
+	int number, even, odd;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+	printf("The number is %d", even_or_odd(number));
+	
+	return 0;
+	}
