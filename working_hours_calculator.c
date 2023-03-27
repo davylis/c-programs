9ea07a6b8c00350ed2b169215e244aa57552746e
davylis <liisamuttonen@gmail.com>
2023-03-27 14:45:03 +0300

    Code ready. Added all functions.

--- working_hours_calculator.c
+++ working_hours_calculator.c
@@ -1,49 +1,43 @@
 #include <stdio.h>
 
-void total_sum(float h[], int size);
-void average (float h[], int size);
+void total_sum_and_average(float h[], int size);
 void entered (float h[], int size);
 
 int main (){
+    float hours[days];
+    int i;
     int days;
 
     printf("The program calculates the total hours worked during"
     "a specific period and the average length of a day.\n\n");
+
     printf("How many days:");
     scanf("%d", &days);
-    float hours[days];
-    int i;
+    
     for (i=1;i<=days;i++){
-    printf("Enter the working hours for day %d:", i);
-    scanf("%f", &hours[i-1]);
+        printf("Enter the working hours for day %d:", i);
+        scanf("%f", &hours[i-1]);
     }
-    total_sum(hours, days);
-    average(hours, days);
+    total_sum_and_average(hours, days);
     entered(hours, days);
 
     return 0;
 }
 
-void total_sum(float h[], int size) {
+void total_sum_and_average(float h[], int size) {
     float sum =0 ;
     int i;
     for (i=0;i<size;i++)
     {
-     sum = h[i] + sum;
+         sum = h[i] + sum;
     }
     printf("Total hours worked: %.1f\n", sum);
-}
 
-void average(float h[], int size) {
-    float sum = 0;
-    for (int i=0;i<size;i++)
-    {
-    sum = h[i] + sum;
-    }
-
-     sum = sum / size;
+    sum = sum / size;
     printf("Average length of day: %.1f\n", sum);
 }
+
+    
 
 void entered(float h[], int size) {
 
