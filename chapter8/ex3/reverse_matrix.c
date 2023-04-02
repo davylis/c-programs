8af2c65015107bf2fdf9bc0f0792250781a64b7c
davylis <liisamuttonen@gmail.com>
2023-04-02 23:09:52 +0300

    New version of file

---
+++ chapter8/ex3/reverse_matrix.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int matrix1[3]= {1, 2, 3};
+
+    int matrix2[3];
+
+    int sum=0;
+    int x, y;
+
+    for (int y=2; x>0; x--)
+    {
+        printf("%d", matrix1[y]);
+    }
+
+return 0;
+
+}
