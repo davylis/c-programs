6d655b44199771b9960a8ec0dffb949130a7ddcb
davylis <liisamuttonen@gmail.com>
2023-04-05 00:42:23 +0300

    Finished file

--- chapter8/ex3/reverse_matrix_with_two_arrays.c
+++ chapter8/ex3/reverse_matrix_with_two_arrays.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 
+#define SIZE_X 3
+#define SIZE_Y 2
+
 int main()
 {
-    int matrix1[2][2]= {1, 2,
-                        3, 4 };
+    int matrix1[2][3]= {{1, 2, 3},
+                        {3, 4, 7 }
+                        };
 
-    int matrix2[2][2];
+    int matrix2[2][3];
 
     int x=0, y;
     int a=0, b;
 
-    for (int y=4; y>=0; y--)
+    for (int y=1; y>=0; y--)
     {
-        for (int a=4; a>=0; a--)
+        for (int b=2; b>=0; b--)
         {
         matrix2[x][a] = matrix1[y][b];
-        x++;
         a++;
         }
+      a = 0;  
+      x++;
     }
 
-    for (int i=0; i<=4; i++)
+    for (int i=0; i < 2; i++)
     {
-        for (int j=0; j<=4; j++)
+        for (int j=0; j < 3; j++)
         {
         printf("%d %d\n", matrix2[i][j], matrix1[i][j]);
         }
