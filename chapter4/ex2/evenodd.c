a11f78d4985ae7088161b620d3e9761b55457276
davylis <liisamuttonen@gmail.com>
2023-04-15 23:24:41 +0300

    Add updated version of program

--- chapter4/ex2/evenodd.c
+++ chapter4/ex2/evenodd.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 
-int even_or_odd (int a)
-{ 
-	int remainder = a % 2;
-	if (remainder == 1){
-		return 1;
-	} else {
-	    return 0;
-	}
-}
-
-int main ()
+int main(void)
 {
-	int number, even, odd;
-    printf("Enter an integer: ");
-    scanf("%d", &number);
-	printf("The number is %d", even_or_odd(number));
-	
-	return 0;
-	}
+  int number = 0;
+  printf("Enter an integer: ");
+  scanf("%d",&number);
+  printf("The number is %d\n",(number%2));
+  return 0;
+}
