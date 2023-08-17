7a7257f6653af87402de521498119868cb8634d6
davylis <liisamuttonen@gmail.com>
2023-08-17 23:18:41 +0300

    Adding program to directory

---
+++ chapter7/ex3/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int largest(int a, int b, int c);
+int smallest(int a, int b, int c);
+
+int main()
+{
+    int num_one, num_two, num_three, max, min;
+    printf("Enter the 1. number:");
+    scanf("%d", &num_one);
+    printf("Enter the 2. number:");
+    scanf("%d", &num_two);
+    printf("Enter the 3. number:");
+    scanf("%d", &num_three);
+    max = largest (num_one, num_two, num_three);
+    min = smallest (num_one, num_two, num_three);
+    printf("Among the numbers you entered,\n"
+    "the largest was %d and the smallest was %d.\n",max, min );
+    return 0;
+}
+
+int largest(int a, int b, int c)
+{
+    if (a > b && a > c) 
+    {
+        return a;
+    }
+    if (b > a && b > c) 
+    {
+        return b;
+    }
+    if (c > b && c > a)
+    {
+        return c;
+    }
+    return 0;
+}
+
+int smallest(int a, int b, int c)
+{
+    if (a < b && a < c) 
+    {
+        return a;
+    }
+    if (b < a && b < c) 
+    {
+        return b;
+    }
+    if (c < b && c < a)
+    {
+        return c;
+    }
+    return 0;
+}
